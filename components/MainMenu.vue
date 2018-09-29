b190c0231e5b859d268ecf8d3c0abb16016c5c4e
Alex Scott <codetheorist@gmail.com>
2018-09-29 12:28:37 +0100

    Update Main Menu

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="links">
-    <a
-      href="https://nuxtjs.org/"
-      target="_blank"
-      class="button--green">Documentation</a>
-    <a
-      href="https://github.com/nuxt/nuxt.js"
-      target="_blank"
-      class="button--grey">GitHub</a>
-  </div>
+  <b-button-group>
+    <b-link
+      class="btn btn-secondary"
+      to="/"
+      exact>Home</b-link>
+    <b-link
+      class="btn btn-secondary"
+      to="about">About</b-link>
+    <b-link
+      class="btn btn-secondary"
+      to="hobbies">Hobbies</b-link>
+    <b-link
+      class="btn btn-secondary"
+      to="gallery">Gallery</b-link>
+    <b-link
+      class="btn btn-secondary"
+      to="games">Games</b-link>
+  </b-button-group>
 </template>
